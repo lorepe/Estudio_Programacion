34979af97a49dd6e68b0da2f6107fdd255906c6b
Jacobo121 <wagner.arcila.jacobo@gmail.com>
2022-01-07 12:25:38 -0500

    Ahora si

--- Clase2/mainL.js
+++ Clase2/mainL.js
@@ -1,29 +1,79 @@
 const natalia = {
-    name:"Natalia",
-    age:6,
-    cursosAprobados:["Curso definitivo de HTML","Curso definitivo de CSS","Curso definitivo de JAVA"],
-    aprobarCurso(nuevoCurso){
-        this.cursosAprobados.push(nuevoCurso)
-    }
+  name: "Natalia",
+  age: 6,
+  cursosAprobados: [
+    "Curso definitivo de HTML",
+    "Curso definitivo de CSS",
+    "Curso definitivo de JAVA",
+  ],
+  aprobarCurso(nuevoCurso) {
+    this.cursosAprobados.push(nuevoCurso);
+  },
 };
 //Hacer que natalia apruebe otro curso
 /* natalia.cursosAprobados.push("")
 natalia.name("") */
- function Student(name,age,cursosAprobados) {
+function Student(name, age, cursosAprobados) {
+  this.name = name;
+  this.age = age;
+  this.cursosAprobados = cursosAprobados;
+  /* this.aprobarCurso = function (nuevoCurso) {
+        this.cursosAprobados.push(nuevoCurso)
+    } */
+}
+//crear un prototipo fuera del objeto(prototipo de funciones)
+Student.prototype.aprobarCurso = function (nuevoCurso) {
+  this.cursosAprobados.push(nuevoCurso);
+};
+
+const juanita = new Student("", 15, [
+  "Curso de Produccion de video Juegos",
+  "Curso de Produccion de Personas",
+]);
+
+//Prototipo con la sintaxis de clases
+class Student2 {
+  constructor(name, age, cursosAprobados) {
     this.name = name;
     this.age = age;
     this.cursosAprobados = cursosAprobados;
-    /* this.aprobarCurso = function (nuevoCurso) {
-        this.cursosAprobados.push(nuevoCurso)
-    } */
   }
- Student.prototype.aprobarCurso = function (nuevoCurso) {
-     this.cursosAprobados.push(nuevoCurso);
- }
- 
- const juanita = new Student(
-     "",
-     15,
-     ["Curso de Produccion de video Juegos",
-     "Curso de Produccion de Personas"]
- )
+}
+
+//Ejercicio Jacobo
+class Mascotas {
+  constructor({ tipo, nombre, edad }) {
+    this.tipo = tipo;
+    this.nombre = nombre;
+    this.edad = edad;
+  }
+
+  
+  comer(hora) {
+    let alive = true;
+    if (hora > 6 && hora < 8) {
+      console.log("Desayunó");
+    } else {
+      console.log("Se murió tu mascota!! :(");
+      alive = false;
+    }
+    return alive;
+  }
+
+
+  dormir(hora) {
+    if (this.comer.alive && hora >= 20) {
+      console.log("Se durmio");
+    } else if (this.comer.alive) {
+      console.log("Es irresponsable, se va de farra");
+    } else if (!this.comer.alive) {
+      console.log("Ya no va a despertar, es un angelito");
+    }
+  }
+}
+//Crear una instancia
+const lorito = new Mascotas({
+  nombre: "Pepe",
+  tipo: "Pajaro",
+  edad: 1,
+});
