492028c48e51775391898bb6baabf352a1ffd2f6
lorepe <loreperez.2003@hotmail.com>
2022-01-07 11:11:09 -0500

    clase2

--- Clase2/mainL.js
+++ Clase2/mainL.js
@@ -1,8 +1,29 @@
 const natalia = {
     name:"Natalia",
     age:6,
-    cursosAprobados:[],
+    cursosAprobados:["Curso definitivo de HTML","Curso definitivo de CSS","Curso definitivo de JAVA"],
+    aprobarCurso(nuevoCurso){
+        this.cursosAprobados.push(nuevoCurso)
+    }
 };
 //Hacer que natalia apruebe otro curso
-natalia.cursosAprobados.push("")
-natalia.name("")
+/* natalia.cursosAprobados.push("")
+natalia.name("") */
+ function Student(name,age,cursosAprobados) {
+    this.name = name;
+    this.age = age;
+    this.cursosAprobados = cursosAprobados;
+    /* this.aprobarCurso = function (nuevoCurso) {
+        this.cursosAprobados.push(nuevoCurso)
+    } */
+  }
+ Student.prototype.aprobarCurso = function (nuevoCurso) {
+     this.cursosAprobados.push(nuevoCurso);
+ }
+ 
+ const juanita = new Student(
+     "",
+     15,
+     ["Curso de Produccion de video Juegos",
+     "Curso de Produccion de Personas"]
+ )
