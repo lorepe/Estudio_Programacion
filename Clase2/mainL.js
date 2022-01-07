16fd6b99acf3049b5caff560ff2cbf981c77c7c3
Jacobo121 <wagner.arcila.jacobo@gmail.com>
2022-01-07 10:47:57 -0500

    Nueva rama

--- Clase2/mainL.js
+++ Clase2/mainL.js
@@ -1,8 +1,12 @@
 const natalia = {
-    name:"Natalia",
-    age:6,
-    cursosAprobados:[],
+    name: "Natalia",
+    age: 20,
+    cursoAprobados: [
+        "Curso Definitivo de HTML y CSS",
+        "Curso Práctico de HTML y CSS"
+    ],
 };
+
 //Hacer que natalia apruebe otro curso
-natalia.cursosAprobados.push("")
-natalia.name("")
+
+natalia.cursoAprobados.push("Curso de Responsive Design")
