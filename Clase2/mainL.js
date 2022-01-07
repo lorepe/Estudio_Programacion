f7bbeba48a17055ba11d557b3d5e0f7aaf044bf5
Jacobo121 <wagner.arcila.jacobo@gmail.com>
2022-01-07 11:10:53 -0500

    Objetos y prototipos

--- Clase2/mainL.js
+++ Clase2/mainL.js
@@ -5,8 +5,39 @@ const natalia = {
         "Curso Definitivo de HTML y CSS",
         "Curso Práctico de HTML y CSS"
     ],
-};
 
+    aprobarCurso(nuevoCursito) {
+        this.cursoAprobados.push(nuevoCursito)
+    }
+};
 //Hacer que natalia apruebe otro curso
 
-natalia.cursoAprobados.push("Curso de Responsive Design")
+//natalia.cursoAprobados.push("Curso de Responsive Design")
+
+natalia.aprobarCurso("Jacobo")
+
+/* ------------------------------------------------------------ */
+
+function Student(name, age, cursoAprobados) {
+    this.name = name;
+    this.age = age;
+    this.cursoAprobados = cursoAprobados;
+    /* this.aprobarCurso = function (nuevoCursito) {
+        this.cursoAprobados.push(nuevoCursito)
+    } */
+}
+
+//creamos metodo desde afuera del prototipo student
+Student.prototype.aprobarCurso = function (nuevoCursito) {
+    this.cursoAprobados.push(nuevoCursito)
+}
+
+//Palabra reservada new para crear un objeto
+const juanita = new Student(
+    "Juanita Alejandra",
+    15,
+    [
+        "Curso de HTML",
+        "Curso de CSS"
+    ]
+)
