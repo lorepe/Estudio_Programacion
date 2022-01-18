a74bc1b5317dc1e39361e9d4492b64488b99cf7b
Jacobo121 <wagner.arcila.jacobo@gmail.com>
2022-01-18 16:54:54 -0500

    promesas

--- Asincronismo/src/callback/challenge.js
+++ Asincronismo/src/callback/challenge.js
@@ -1,5 +1,5 @@
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-const API = 'https://rickandmortyapi.com/api/characte/'
+const API = 'https://rickandmortyapi.com/api/character/'
 
 function fetchData(url_api, callback) {
     let xhttp = new XMLHttpRequest();
---
+++ Asincronismo/src/promise/index.js
@@ -0,0 +1,42 @@
+
+const somethingWillHappen = () => {
+    return new Promise((resolve, reject) => {
+        if(true) {
+            resolve('hey')
+        } else {
+            reject('whoooooooooops!')
+        }
+    });
+};
+
+somethingWillHappen()
+.then(response => console.log(response))
+.catch(err => console.error(err));
+
+
+
+const somethingWillHappen2 = () => {
+     return new Promise((resolve, reject) => {
+         if(true) {
+             setTimeout(() => {
+                resolve('true');
+            }, 2000);
+        } else {
+            const error = new Error('WHOOOP')
+            reject(error);
+        }
+    });
+};
+
+somethingWillHappen2()
+.then(response => console.log(response))
+.catch(err => console.error(err));
+
+
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+    .then(response => {
+        console.log("array xd", response)
+    })
+    .catch(err => {
+        console.log(err)
+    })
