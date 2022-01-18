4168fb3f12b72afbb1dcdc019a7820d8aafdf080
lorepe <loreperez.2003@hotmail.com>
2022-01-18 17:41:40 -0500

    Repaso_14_01_2022

--- Asincronismo/src/async/index.js
+++ Asincronismo/src/async/index.js
@@ -1,17 +1,30 @@
-const doSometimeAsync = () => {
+const doSomethingAsync = () => {
   return new Promise((resolve, reject) => {
     true
       ? setTimeout(() => {
           resolve("Do Something Async");
         }, 3000)
-      : reject(new Error("TextError"));
+      : reject(new Error("Text Error"));
   });
 };
 
 const doSomething = async () => {
-  const something = await doSomething();
+  const something = await doSomethingAsync();
   console.log(something);
 };
 console.log("Before");
 doSomething();
 console.log("After");
+
+
+const anotherFunction = async ()=>{
+  try{
+    const something = await doSomethingAsync();
+    console.log(something)
+  }catch(error){
+    console.error(error)
+  }
+}
+console.log("Before 1");
+anotherFunction();
+console.log("After 1");
